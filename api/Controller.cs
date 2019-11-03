e201e5087dbf92f6d4f4b6df66f73e02ccdc83fe
Michael Wildman <michael@mwild.me>
2019-11-03 23:15:36 +1300

    initial commit

---
+++ api/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sqlweb.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class Controller : ControllerBase
+    {
+        private readonly ILogger logger;
+
+        public Controller(ILoggerFactory logger)
+        {
+            this.logger = logger.CreateLogger("sqlweb");
+        }
+       
+        [HttpPost]
+        [Route("query")]
+        public IActionResult RunQuery([FromBody] RunQueryRequest request)
+        {
+            logger.LogInformation(request?.Query);
+
+            var results = new[] {
+                new FakeData(1, "Hello", "world"),
+                new FakeData(2, "Something", "blah"),
+            };
+
+            return Ok(results);
+        }
+    }
+
+
+
+    public class FakeData
+    {
+        public FakeData(int id, string name, string foo)
+        {
+            Id = id; Name = name; Foo = foo;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Foo { get; set; }
+    }
+
+
+    public class RunQueryRequest {
+        public string Query { get; set; }
+    }
+
+    
+}
---
+++ static/js/app.js
@@ -0,0 +1,85 @@
+import 'ace-builds/src-noconflict/ace';
+import 'ace-builds/src-noconflict/mode-sqlserver';
+import 'ace-builds/src-noconflict/theme-tomorrow';
+import 'ace-builds/src-noconflict/theme-github';
+
+import _ from 'lodash';
+import * as http from 'axios';
+import Vue from 'vue/dist/vue';
+
+
+// var editor = ace.edit('input');
+
+
+// editor.setFontSize(13);
+// editor.setTheme('ace/theme/tomorrow');
+// editor.setShowPrintMargin(false);
+// editor.session.setMode('ace/mode/sqlserver');
+// editor.session.setTabSize(2);
+// editor.session.setUseSoftTabs(true);
+
+// editor.commands.addCommands([{
+//   name: 'run_query',
+//   bindKey: {
+//     win: 'Ctrl-Enter',
+//     mac: 'Command-Enter'
+//   },
+//   exec: function(editor) {
+//     runQuery(editor);
+//   }
+// }]);
+
+
+
+// function runQuery(editor) {
+//   var query = _.trim(editor.getSelectedText() || editor.getValue());
+
+//   console.log('runQuery: %s', query);
+
+//   http.post('/api/query', { query })
+//     .then(function(res) {
+      
+//     });
+// }
+
+
+
+Vue.component('editor', {
+  template: '<div :id="editorId"></div>',
+  props: ['editorId', 'content'],
+  data: function() {
+    return {
+      editor: Object,
+      prevContent: ''
+    }
+  },
+  watch: {
+    'content': function(value) {
+      if (this.prevContent !== value) {
+        this.editor.setValue(value, 1);
+      }
+    }
+  },
+  mounted: function() {
+    this.editor = window.ace.edit(this.editorId);
+    this.editor.setValue(this.content, 1);
+
+    this.editor.getSession().setMode('ace/mode/sqlserver');
+    this.editor.setTheme('ace/theme/tomorrow');
+
+    this.editor.on('change', function() {
+      this.prevContent = this.editor.getValue();
+      this.$emit('change-content', this.editor.getValue());
+    })
+  }
+});
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    results: []
+  }
+});
+
+
